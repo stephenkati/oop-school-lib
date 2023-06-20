c0a1d2719f2ab687399e11ce894e23911239d6d3
stephenkati <stephenkatuli88@gmail.com>
2023-06-20 11:37:21 +0300

    update main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
-require_relative 'Person'
-require_relative 'Student'
-require_relative 'Teacher'
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
 
 person = Person.new(27)
 puts person.name
@@ -23,3 +25,10 @@ person_without_permission = Person.new(16, parent_permission: false)
 
 puts person_with_permission.can_use_services?
 puts person_without_permission.can_use_services?
+
+person = Person.new(22, name: 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
