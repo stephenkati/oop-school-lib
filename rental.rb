a3fa960120fd8445dd6906d96771b24be2e46552
stephenkati <stephenkatuli88@gmail.com>
2023-06-21 12:12:55 +0300

    update rental.rb

--- rental.rb
+++ rental.rb
@@ -1,10 +1,16 @@
 class Rental
-  attr_accessor :date, :book, :person
+  attr_accessor :id, :date, :book, :person
 
   def initialize(date, book, person)
+    @id = Data.generate_rental_id
     @date = date
     assign_book(book)
     assign_person(person)
+    Data.rentals << self
+  end
+
+  def self.find_by_person_id(person_id)
+    Data.rentals.select { |rental| rental.person.id == person_id }
   end
 
   private
