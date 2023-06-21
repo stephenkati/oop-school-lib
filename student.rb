b6bba6a28bf8bcfbe98b39ac054c3e50209110b5
stephenkati <stephenkatuli88@gmail.com>
2023-06-21 10:49:14 +0300

    add classroom association

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :name, :classroom
+
   def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, parent_permission: parent_permission, name: name)
     @classroom = classroom
@@ -8,5 +10,11 @@ class Student < Person
 
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def new_classroom=(classroom)
+    @classroom&.remove_student(self)
+    @classroom = classroom
+    classroom&.add_student(self)
   end
 end
