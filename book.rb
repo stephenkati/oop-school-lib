31cfdada8a4da2a5bb32e58dd810fadf4e4ea090
stephenkati <stephenkatuli88@gmail.com>
2023-06-21 10:48:13 +0300

    create book.rb

---
+++ book.rb
@@ -0,0 +1,13 @@
+
+class Book
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+
+  def rentals
+    Rental.all.select { |rental| rental.book == self }
+  end
+end
