5a4bf64ca90d4e9363ae80ad7b66aecc269a4bc3
stephenkati <stephenkatuli88@gmail.com>
2023-06-27 19:10:44 +0300

    make changes requested

--- preserve_data.rb
+++ preserve_data.rb
@@ -14,7 +14,8 @@ def file_read(file)
 end
 
 def file_write(file, content)
-  hashed_content = content.map(&:to_hash)
-  json = JSON.generate(hashed_content)
+  existing_data = file_read(file)
+  all_data = existing_data + content.map(&:to_hash)
+  json = JSON.generate(all_data)
   File.write(file, json)
 end
